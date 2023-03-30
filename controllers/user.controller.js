79b593edebdc596895072bda878e3f0ba372a158
sara823 <66432441+sara823@users.noreply.github.com>
2023-03-30 11:43:54 +0200

    getReviews

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,2 +1,9 @@
 import { reviews } from '../models/reviews.js'
 import { validateReview } from '../helpers/validation'
+export const getReviews = (req, res) => {
+    const Reviews = reviews
+    if (!reviews) {
+      res.send("there's no reviews")
+    }
+    res.status(200).send(Reviews)
+  }
