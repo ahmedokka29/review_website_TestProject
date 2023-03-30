4c2635c39c42eb064fa69e2d63acd20fcdae7a7e
Ahmed Gamal Okka <56696104+ahmedokka29@users.noreply.github.com>
2023-03-30 21:57:49 +0200

    Update Validation.js

--- helpers/validation.js
+++ helpers/validation.js
@@ -1,1 +1,12 @@
 import Joi from 'joi'
+
+export const validateReview = (review) => {
+  const schema = Joi.object({
+    id: Joi.number().integer().min(1),
+    title: Joi.string(),
+    description: Joi.string(),
+    rating: Joi.number().integer().min(1).max(10),
+  })
+
+  return schema.validate(review)
+}
