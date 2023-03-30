9710b65a79de56a7f45894d01659aa8453f0746b
sara823 <66432441+sara823@users.noreply.github.com>
2023-03-30 12:19:48 +0200

    addingroute

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,4 +1,13 @@
 import { Router } from 'express'
+<<<<<<< Updated upstream
+=======
+import {  getReviewById, addReview,getReviews} from '../controllers/user.controller.js'
+const reviewRouter = Router()
+reviewRouter.get('/api/reviews', getReviews)
+reviewRouter.get('/api/reviews/:id', getReviewById)
+reviewRouter.post('/api/reviews', addReview)
+export { reviewRouter }
+>>>>>>> Stashed changes
 
 const reviewRouter = Router()
 
