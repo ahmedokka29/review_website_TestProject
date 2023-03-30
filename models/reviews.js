3a84143f01f7d2c7d57964df262ae8d49c342f96
sara823 <66432441+sara823@users.noreply.github.com>
2023-03-30 11:34:25 +0200

    addingData

--- models/reviews.js
+++ models/reviews.js
@@ -1,3 +1,40 @@
-let reviews = []
+let reviews = [
+  {
+    id: 1,
+    title: 'First Review',
+    description: 'This is the first review',
+    rating: 4,
+  },
+  {
+    id: 2,
+    title: 'Second Review',
+    description: 'This is the second review',
+    rating: 5,
+  },
+  {
+    id: 3,
+    title: 'Third Review',
+    description: 'This is the third review',
+    rating: 3,
+  },
+  {
+    id: 4,
+    title: 'fourth Review',
+    description: 'This is the forth review',
+    rating: 9,
+  },
+  {
+    id: 5,
+    title: 'fifth Review',
+    description: 'This is the forth review',
+    rating: 10,
+  },
+  {
+    id: 6,
+    title: 'six Review',
+    description: 'This is the forth review',
+    rating: 8,
+  },
+]
 
 export { reviews }
