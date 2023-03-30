c667a2d084f91a99b378060529563251c5bd1d45
Rana Osama <66523119+RanaUsama20@users.noreply.github.com>
2023-03-30 11:52:01 +0200

    addingRoute

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
-
+import {  getReviewById} from '../controllers/user.controller.js'
 const reviewRouter = Router()
-
+reviewRouter.get('/api/reviews/:id', getReviewById)
 export { reviewRouter }
+
