dda4a2ef43181dc15c2c8afc52bf4f51b7cd08d4
shourok-abdullah <95624488+Shrouk-Abdullah@users.noreply.github.com>
2023-03-30 12:26:50 +0200

    edittingReview

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,33 +1,46 @@
 import { reviews } from '../models/reviews.js'
 import { validateReview } from '../helpers/validation'
+
 export const getReviews = (req, res) => {
-    const Reviews = reviews
-    if (!reviews) {
-      res.send("there's no reviews")
-    }
-    res.status(200).send(Reviews)
+  const Reviews = reviews
+  if (!reviews) {
+    res.send("there's no reviews")
   }
-  export const getReviewById = (req, res) => {
-    const id = req.params.id
-    const review = reviews.filter((f) => f.id == id)
-    if (!review) {
-      res.send("there's no review by this ID")
-    }
-    res.status(200).send(review)
+  res.status(200).send(Reviews)
+}
+export const getReviewById = (req, res) => {
+  const id = req.params.id
+  const review = reviews.filter((f) => f.id == id)
+  if (!review) {
+    res.send("there's no review by this ID")
   }
-  export const addReview = (req, res) => {
-    const review = req.body
-    const validateRusult = validateReview(review)
-    if (validateRusult.error) {
-      res.status(400).send(validateRusult.error.details[0].message)
-    }
-    const addedReview = {
-      id: reviews.length + 1,
-      title: req.body.title,
-      description: req.body.description,
-      rating: req.body.rating,
-    }
-  
-    reviews.push(addedReview)
-    res.status(201).send(addedReview)
+  res.status(200).send(review)
+}
+export const addReview = (req, res) => {
+  const review = req.body
+  const validateRusult = validateReview(review)
+  if (validateRusult.error) {
+    res.status(400).send(validateRusult.error.details[0].message)
   }
+  const addedReview = {
+    id: reviews.length + 1,
+    title: req.body.title,
+    description: req.body.description,
+    rating: req.body.rating,
+  }
+
+  reviews.push(addedReview)
+  res.status(201).send(addedReview)
+}
+export const editReview = (req, res) => {
+  const id = req.params.id
+  const review = reviews.find((c) => c.id === parseInt(req.params.id))
+  const validateRusult = validateReview(req.body)
+  if (validateRusult.error) {
+    res.status(400).send(validateRusult.error.details[0].message)
+  }
+  review.title = req.body.title
+  review.description = req.body.description
+  review.rating = req.body.rating
+  res.status(200).send(review)
+}
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,8 +1,16 @@
 import { Router } from 'express'
-import {  getReviewById, addReview,getReviews} from '../controllers/user.controller.js'
+import {
+  getReviewById,
+  addReview,
+  getReviews,
+  editReview,
+} from '../controllers/user.controller.js'
+
 const reviewRouter = Router()
+
 reviewRouter.get('/api/reviews', getReviews)
 reviewRouter.get('/api/reviews/:id', getReviewById)
 reviewRouter.post('/api/reviews', addReview)
-export { reviewRouter }
+reviewRouter.put('/api/reviews/:id', editReview)
 
+export { reviewRouter }
