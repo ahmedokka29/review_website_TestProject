0788c5f54b7c08ba427ba27d5cd5311457ad250b
shrouk-abdullah <https://github.com/Shrouk-Abdullah>
2023-04-13 16:21:52 +0200

    connecting to mongo

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from 'express'
-import { reviewRouter } from './routes/reviewRoutes'
+import { reviewRouter } from './routes/reviewRoutes.js'
 
-const port = process.env.PORT || 8080
+import dotenv from 'dotenv'
+
+dotenv.config()
 const app = express()
+
+const port = process.env.PORT
+const url = process.env.DB
 
 app.use(express.json())
 
 app.use(reviewRouter)
 
 app.listen(port, async () => {
+  await mongoose.connect(`${url}`)
   console.log(`hello from port ${port}`)
 })
