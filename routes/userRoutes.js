ac8f70984f51c397c6761c0d477f8c7b0e49d8e7
Ahmed Gamal Okka <ahmed.okka@hotmail.com>
2023-04-19 00:44:26 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import {
-  getUsers,
+  getUser,
   signup,
   getUsers
 } from '../controllers/userController.js';
 const userRoutes = Router();
-userRoutes.get('/api/users/info', getUsers);
+userRoutes.get('/api/users/info', getUser);
 userRoutes.post('/api/users/signup', signup);
 userRoutes.get('/api/users', getUsers);
 export { userRoutes };
